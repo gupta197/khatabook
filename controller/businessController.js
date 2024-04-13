9bb65385959da6863fa7254ab3f3a298efb088ff
gupta197 <vg2292097@gmail.com>
2024-04-13 23:42:50 +0530

    Create api to update business detail

--- controller/businessController.js
+++ controller/businessController.js
@@ -41,21 +41,15 @@ module.exports = {
                 });
             }
             const oldDetails = await BussinessDetail.findOne({ userId });
-
+            req.body.userId = userId;
             if (oldDetails) {
                 return res.status(409).send({
                     success: false,
                     message: "Bussiness Detail Already Exist. ",
                 });
             }
-            const bussinessDetail = await BussinessDetail.create({
-                businessName,
-                contactNumber,
-                address,
-                businessType,
-                userId,
-                additionalDetail
-            });
+            
+            const bussinessDetail = await BussinessDetail.create(req.body);
 
             return res.status(201).send({
                 success: true,
@@ -71,11 +65,31 @@ module.exports = {
             });
         }
     },
-    updateBussinessDetail2: async (req, res) => {
+    updateBussinessDetail: async (req, res) => {
         try {
-            const { userId } = req.user;
-            const { businessName, contactNumber, address, businessType, additionalDetail } = req.body;
-            const { id } = req.params;
+            const { id,businessName, contactNumber, address, businessType, additionalDetail } = req.body;
+            if(commonFunctions.checkBlank([id])){
+                return res.status(400).send({
+                    success: false,
+                    message: error.message,
+                });
+            }
+            const oldDetails = await BussinessDetail.findOne({ _id : id });
+            if(!oldDetails){
+                return res.status(409).send({
+                    success: false,
+                    message: "Bussiness Detail Not Exist. ",
+                });
+            }
+            delete req.body.userId;
+            let bussinessDetail = await BussinessDetail.updateOne(
+                { _id: id }, req.body
+              )
+              return res.status(200).send({
+                success: true,
+                message: "Business Detail Updated successfully!!",
+                bussinessDetail
+            });
 
         } catch (error) {
             return res.status(500).send({
--- routes/businessRoute.js
+++ routes/businessRoute.js
@@ -3,6 +3,8 @@ const router = require('express').Router(),
  auth = require("../middleware/auth");
 // Get Business Detail
 router.get('/',auth, businessControllerController.getBussinessDetail);
+router.post('/',auth, businessControllerController.CreateBussinessDetail);
+router.put('/',auth, businessControllerController.updateBussinessDetail);
 
 
 module.exports = router;
