e6d8fa95882d8a11342442fb9593564afb86f48f
gupta197 <vg2292097@gmail.com>
2024-04-18 20:51:29 +0530

    add comment on api

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -7,11 +7,16 @@ module.exports = {
     // Get all transaction with respect to user and get transaction detail
     getTransactions : async (req, res)=>{
         try {
+            // Get The User id from JWT 
             const { userId } = req.user;
+            // Create object to filter out the records in single api
             let getDetailObj = {
                 userId
             }
+            //Check the user id is not blank
             if (commonFunctions.checkBlank([userId])) {
+
+                //Sending response to end user
                 return res.status(400).send({
                     success: false,
                     message: "Bad Request",
@@ -26,13 +31,18 @@ module.exports = {
             if(req.query.id && commonFunctions.checkBlank([req.query.id])){
                 getDetailObj.transactionId = req.query.id;
             }
+            // Fetching the transactions
             const transactions = await Transaction.find(getDetailObj);
+
+            //Sending response to end user
             return res.status(200).send({
                 success: true,
-                message: "All Transaction Fetched sucessfully",
+                message: "Transaction Fetched sucessfully",
                 records: transactions
               });
+
         } catch (error) {
+            //Sending response to end user
             return res.status(500).send({
                 success: false,
                 message: error.message,
@@ -43,23 +53,32 @@ module.exports = {
     // Generate new Transaction with respect to user
     createTransaction : async (req, res)=>{
         try {
+            // Get The User id from JWT 
             const { userId } = req.user;
             // these field are used to create the customer amount, type , customerId, address
             let { amount, type , customerId} = req.body;
 
             // Check the type should be credit or debit
             type  = type == "credit" ? "credit" : "debit";
+            // Validate the required detail should not be empty or be null
             if(commonFunctions.checkBlank([amount, type , customerId])){
+                //Sending response to end user
                 return res.status(400).send({
                     success: false,
                     message: "Parameter missing.. !!",
                 });
             }
+            //Check Customer is exist with respect to user
             let checkCustomer = await Customer.findOne({customerId , userId});
+
+            //Validate the customer get from db
             if(checkCustomer && checkCustomer.length){
                 req.body.transactionId = new mongoose.Types.ObjectId().toString();
                 req.body.userId = userId;
+
+                // Create new Transaction
                 const transactions = await Transaction.create(req.body);
+                //send Response to end user
                 res.status(200).send({
                     success: true,
                     message: "Transaction Created successfully",
@@ -76,9 +95,12 @@ module.exports = {
                 message: error.message,
               });
         }
-    }, 
+    },
+
+    // Update the transaction details
     updateTransaction : async (req, res)=>{
         try {
+            // Get The User id from JWT 
             const { userId } = req.user;
             // these field are used to create the customer amount, type , customerId, address
             let { transactionId } = req.body;
@@ -117,7 +139,7 @@ module.exports = {
                 success: true,
                 message: "Transaction update successfully",
               });
-              
+
         } catch (error) {
             //Sending the response to End user or requested user
             return res.status(500).send({
@@ -126,8 +148,11 @@ module.exports = {
               });
         }
     },
+    
+    // Delete the particular transaction
     deleteTransaction : async (req, res)=>{
         try {
+            // Get The User id from JWT 
             const { userId } = req.user;
             // these field are used to create the customer amount, type , customerId, address
             const { transactionId } = req.body;
