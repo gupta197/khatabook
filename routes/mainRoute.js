6e8d9da497128b1ad1b59165b461cdd834134e4c
gupta197 <vg2292097@gmail.com>
2024-04-19 22:04:35 +0530

    Add test api swagger route added

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -9,6 +9,28 @@ auth = require('../controller/authContoller');
 // Register
 router.post("/register", auth.register);
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     description: Login to the application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: username
+ *         description: Username to use for login.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: login
+ */
 // Login and setup 2 factor authication
 router.post("/login",auth.login);
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,28 +1,7 @@
 const router = require('express').Router();
 const {setup2fa,getUserDetail} = require('../controller/userController');
 const auth = require("../middleware/auth");
-/**
- * @swagger
- * /login:
- *   post:
- *     description: Login to the application
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: username
- *         description: Username to use for login.
- *         in: formData
- *         required: true
- *         type: string
- *       - name: password
- *         description: User's password.
- *         in: formData
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: login
- */
+
 // Enable and disble the 2fA
 router.post('/2fa',auth,setup2fa);
 // Get User Detail
