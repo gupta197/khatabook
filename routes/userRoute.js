1a227d994ddc0ffa9219e12ea4d1e33b1d035377
gupta197 <vg2292097@gmail.com>
2024-04-24 22:41:15 +0530

    Add Swagger for user 2FA api

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,33 @@ const router = require('express').Router();
 const {setup2fa,getUserDetail} = require('../controller/userController');
 const auth = require("../middleware/auth");
 
+/**
+ * @swagger
+ * /user/2fa:
+ *   post:
+ *     description: API is use to enable/disbale the 2 factor authication
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: x-access-token
+ *         description: an authorization header
+ *         in: header
+ *         required: true
+ *         type: string
+ *       - name: flag
+ *         description: Flag is used for enable the 2 factor authicaation, it should be boolean value
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: 2FA is alread enabled or disabled successfully
+ *       400:
+ *         description: Bad Request / no user found with such userId!!!
+ *       500:
+ *         description: Something went wrong!...
+ */
+
 // Enable and disble the 2fA
 router.post('/2fa',auth,setup2fa);
 // Get User Detail
