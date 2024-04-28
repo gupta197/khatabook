64b7c509778fb86d3f99e4bc10b7cf7f7291a3b9
gupta197 <vg2292097@gmail.com>
2024-04-28 22:10:02 +0530

    Add get transaction detail api swagger documentation

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -2,9 +2,43 @@ const router = require('express').Router(),
 transactionController = require('../controller/transactionController'),
  auth = require("../middleware/auth");
 
+ 
 // Create New Transaction
 router.post('/',auth, transactionController.createTransaction);
 
+/**
+ * @swagger
+ * /transaction:
+ *   get:
+ *     description: API is use to get the all transaction and transaction detail
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: x-access-token
+ *         description: an authorization header
+ *         in: header
+ *         required: true
+ *         type: string
+ *       - name: id
+ *         description: transaction id that you need to check the detail
+ *         in: query
+ *         required: false
+ *         type: string
+*       - name: customerId
+ *         description: Customer id that you need to check the transaction detail
+ *         in: query
+ *         required: false
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: transaction Found Successfully
+ *       400:
+ *         description: Bad Request 
+ *       404:
+ *         description: transactions not found
+ *       500:
+ *         description: Something went wrong!...
+ */
 // Get All transaction or get particular transaction
 router.get('/',auth, transactionController.getTransactions);
 
